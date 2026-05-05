102a54dd6b548e09036493f4249d33dea2fd0547
Yossmel Bruno <yossmelbrunoamado@gmail.com>
2026-05-05 18:35:39 -0500

    Cliente backend terminado

--- backend/routes/clienteRoutes.js
+++ backend/routes/clienteRoutes.js
@@ -1,1 +1,10 @@
-dsfdfss
+const router = express.Router();
+const controller = require("../controllers/clienteControllers");
+
+router.get("/", controller.obtenerClientes);
+router.get("/:id", controller.obtenerCliente);
+router.post("/", controller.crearCliente);
+router.put("/:id", controller.actualizarCliente);
+router.delete("/:id", controller.eliminarCliente);
+
+module.exports = router;
